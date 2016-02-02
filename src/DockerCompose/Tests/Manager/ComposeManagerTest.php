40d1c56917d509cfb445290132fc462836cf25a6
ophelie.mauger <ophelie.mauger@alterway.fr>
2016-02-02 15:13:32 +0100

    Add command restart

--- src/DockerCompose/Tests/Manager/ComposeManagerTest.php
+++ src/DockerCompose/Tests/Manager/ComposeManagerTest.php
@@ -393,4 +393,54 @@ class ComposeManagerTest extends PHPUnit_Framework_TestCase
         $this->manager->method('execute')->with('docker-compose build --pull --force-rm --no-cache')->willReturn(array('output' => 'ok', 'code' => 0));
         $this->assertEquals($this->manager->build([], true, true, false), 'ok');
     }
+
+    /**
+     * Test simple restart without error
+     */
+    public function testRestart()
+    {
+        $this->manager->method('execute')->with('docker-compose restart')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->restart(), 'ok');
+    }
+
+    /**
+     * Test simple restart with timeout
+     */
+    public function testRestartWithTimeout()
+    {
+        $this->manager->method('execute')->with('docker-compose restart --timeout=30')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->restart([], 30), 'ok');
+    }
+
+    /**
+     * Test restart success with one compose file
+     */
+    public function testRestartWithOneComposeFileSpecified()
+    {
+        $this->manager->method('execute')->with('docker-compose -f docker-compose.test.yml restart')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->restart('docker-compose.test.yml'), 'ok');
+    }
+
+    /**
+     * Test restart success with two compose files
+     */
+    public function testRestartWithTwoComposeFilesSpecified()
+    {
+        $this->manager->method('execute')->with('docker-compose -f docker-compose.yml -f docker-compose.test.yml restart')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->restart(['docker-compose.yml', 'docker-compose.test.yml']), 'ok');
+    }
+
+    /**
+     * Test restart with project option
+     */
+    public function testRestartWithprojectOption()
+    {
+        $composeFiles = new ComposeFileCollection(['docker-compose.test.yml']);
+        $composeFiles->setProjectName('unittest');
+
+        $this->manager->method('execute')->with('docker-compose -f docker-compose.test.yml --project-name unittest restart')->willReturn(array('output' => 'ok', 'code' => 0));
+
+        $this->assertEquals($this->manager->restart($composeFiles), 'ok');
+
+    }
 }
