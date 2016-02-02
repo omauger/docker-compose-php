c821516446261b4c54c746e2f5648b98ed5a4a31
ophelie.mauger <ophelie.mauger@alterway.fr>
2016-02-02 14:19:47 +0100

    Add parameters --pull, --force-rm and --no-cache for build command

--- src/DockerCompose/Tests/Manager/ComposeManagerTest.php
+++ src/DockerCompose/Tests/Manager/ComposeManagerTest.php
@@ -336,7 +336,7 @@ class ComposeManagerTest extends PHPUnit_Framework_TestCase
      */
     public function testBuild()
     {
-        $this->manager->method('execute')->with('docker-compose build')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->manager->method('execute')->with('docker-compose build --pull')->willReturn(array('output' => 'ok', 'code' => 0));
         $this->assertEquals($this->manager->build(), 'ok');
     }
 
@@ -345,7 +345,7 @@ class ComposeManagerTest extends PHPUnit_Framework_TestCase
      */
     public function testBuildWithOneComposeFileSpecified()
     {
-        $this->manager->method('execute')->with('docker-compose -f docker-compose.test.yml build')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->manager->method('execute')->with('docker-compose -f docker-compose.test.yml build --pull')->willReturn(array('output' => 'ok', 'code' => 0));
         $this->assertEquals($this->manager->build('docker-compose.test.yml'), 'ok');
     }
 
@@ -354,7 +354,43 @@ class ComposeManagerTest extends PHPUnit_Framework_TestCase
      */
     public function testBuildWithTwoComposeFilesSpecified()
     {
-        $this->manager->method('execute')->with('docker-compose -f docker-compose.yml -f docker-compose.test.yml build')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->manager->method('execute')->with('docker-compose -f docker-compose.yml -f docker-compose.test.yml build --pull')->willReturn(array('output' => 'ok', 'code' => 0));
         $this->assertEquals($this->manager->build(['docker-compose.yml', 'docker-compose.test.yml']), 'ok');
+    }
+
+    /**
+     * Test simple build without pull
+     */
+    public function testBuildWithoutPull()
+    {
+        $this->manager->method('execute')->with('docker-compose build')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->build([], false), 'ok');
+    }
+
+    /**
+     * Test simple build with --force-rm
+     */
+    public function testBuildWithForceRm()
+    {
+        $this->manager->method('execute')->with('docker-compose build --force-rm')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->build([], false, true), 'ok');
+    }
+
+    /**
+     * Test simple build with --no-cache
+     */
+    public function testBuildWithNoCache()
+    {
+        $this->manager->method('execute')->with('docker-compose build --no-cache')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->build([], false, false, false), 'ok');
+    }
+
+    /**
+     * Test simple build with --pull --force-rm --no-cache
+     */
+    public function testBuildWithPullAndForceRmAndNoCache()
+    {
+        $this->manager->method('execute')->with('docker-compose build --pull --force-rm --no-cache')->willReturn(array('output' => 'ok', 'code' => 0));
+        $this->assertEquals($this->manager->build([], true, true, false), 'ok');
     }
 }
